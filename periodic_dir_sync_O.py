4daa631f480b877bf823250af44f6db8b595cdac
jupiterbjy <jupiterbjy@gmail.com>
2026-01-10 20:03:15 +0900

    SingleScriptTools dependency sync

---
+++ periodic_dir_sync_O.py
@@ -0,0 +1,164 @@
+"""
+Syncs two independent directories periodically, with file extension whitelist.
+
+Used to sync dependant resources real time while writing my own static webpage builder for github.io page.
+
+![](readme_res/periodic_dir_sync.jpg)
+
+:Auther: jupiterbjy@gmail.com
+"""
+
+import pathlib
+import time
+from argparse import ArgumentParser
+from collections.abc import Sequence
+
+# --- Utilities ---
+
+CHANGES_FORMAT = "f+ {} / f- {} / d+ {} / d- {}"
+
+
+def print_changes(change_tuple: tuple[int, int, int, int]):
+    print(CHANGES_FORMAT.format(*change_tuple))
+
+
+# --- Logics ---
+
+
+def multi_src_sync_dir(
+    src_roots: Sequence[pathlib.Path],
+    dest_root: pathlib.Path,
+    src_excl: set[str] = None,
+) -> tuple[int, int, int, int]:
+    """Syncs all files from src to dest excluding file extensions in excluded_exts.
+    Does not care about overlapping file name in multiple src roots.
+
+    Args:
+        src_roots: Sequence of Source root dirs
+        dest_root: Destination's root dir
+        src_excl: Excluded files from source's root dir
+
+    Returns:
+        (created file count, deleted file count, created dir count, deleted dir count)
+    """
+
+    # since we can't really use watchdog.utils.dirsnapshot due to diff dir we go manual
+    src_excl = src_excl or set()
+
+    all_dest_paths = set(p.relative_to(dest_root) for p in dest_root.rglob("*"))
+    all_src_paths = set()
+
+    # merge src paths into one
+    for src_root in src_roots:
+        all_src_paths.update(
+            p.relative_to(src_root)
+            for p in src_root.rglob("*")
+            if p.suffix not in src_excl
+        )
+
+    dangling_dest_paths = {p for p in (all_dest_paths - all_src_paths)}
+
+    # delete extra paths
+    f_deletes = 0
+    d_deletes = 0
+
+    for root, dir_names, file_names in dest_root.walk(top_down=False):
+
+        for fn in file_names:
+            path = root / fn
+            if path.relative_to(dest_root) not in dangling_dest_paths:
+                continue
+
+            f_deletes += 1
+            (root / fn).unlink()
+
+        for dn in dir_names:
+            path = root / dn
+            if path.relative_to(dest_root) not in dangling_dest_paths:
+                continue
+
+            d_deletes += 1
+            (root / dn).rmdir()
+
+    # copy new paths if missing or different with mtime
+    f_creates = 0
+    d_creates = 0
+
+    for src_root in src_roots:
+        for root, dir_names, file_names in src_root.walk():
+
+            for fn in file_names:
+
+                src_path = root / fn
+
+                if src_path.suffix in src_excl:
+                    continue
+
+                dest_path = dest_root / src_path.relative_to(src_root)
+
+                if not dest_path.exists():
+                    src_path.copy(dest_path, preserve_metadata=True)
+                    f_creates += 1
+                    continue
+
+                src_stat = src_path.stat()
+                dest_stat = dest_path.stat()
+
+                if (
+                    src_stat.st_mtime != dest_stat.st_mtime
+                    or src_stat.st_size != dest_stat.st_size
+                ):
+                    src_path.copy(dest_path, preserve_metadata=True)
+                    f_creates += 1
+
+            for dn in dir_names:
+                src_path = root / dn
+                dest_path = dest_root / src_path.relative_to(src_root)
+
+                if not dest_path.exists():
+                    dest_path.mkdir(exist_ok=True)
+                    d_creates += 1
+
+    return f_creates, f_deletes, d_creates, d_deletes
+
+
+# --- Drivers ---
+
+
+def periodic_sync(src_root: pathlib.Path, dest_root: pathlib.Path, interval: float):
+    while True:
+        changes = multi_src_sync_dir([src_root], dest_root)
+
+        if sum(changes) > 0:
+            print(f"{time.time():.1f}: ", end="")
+            print_changes(changes)
+
+        time.sleep(interval)
+
+
+if __name__ == "__main__":
+    _parser = ArgumentParser()
+    _parser.add_argument(
+        "src_root",
+        type=pathlib.Path,
+        help="Path to the source directory",
+    )
+    _parser.add_argument(
+        "dest_root",
+        type=pathlib.Path,
+        help="Path to the destination directory",
+    )
+    _parser.add_argument(
+        "-t",
+        "--time-interval",
+        type=float,
+        default=1,
+        help="Time interval in seconds between syncs",
+    )
+
+    _args = _parser.parse_args()
+
+    try:
+        periodic_sync(_args.src_root, _args.dest_root, _args.time_interval)
+    except KeyboardInterrupt:
+        pass
--- watchdog_file_events_m.py
+++ watchdog_file_events_m.py
@@ -37,24 +37,50 @@ with start_watchdog(str(pathlib.Path(__file__).parent.absolute()), True) as hand
 import time
 import pathlib
 from contextlib import contextmanager
-from typing import Callable, List, Iterator
+from typing import Callable, List
 from collections import defaultdict
+from collections.abc import Iterable, Iterator
 
 from watchdog.observers import Observer
 from watchdog.events import (
     FileSystemEventHandler,
     FileSystemEvent,
-
     FileCreatedEvent,
     FileDeletedEvent,
     FileMovedEvent,
     FileModifiedEvent,
-
     DirCreatedEvent,
     DirDeletedEvent,
     DirMovedEvent,
     DirModifiedEvent,
 )
+
+# from watchdog.utils.dirsnapshot import DirectorySnapshot, DirectorySnapshotDiff
+
+
+__all__ = [
+    "FileSystemEvent",
+    "FileCreatedEvent",
+    "FileDeletedEvent",
+    "FileMovedEvent",
+    "FileModifiedEvent",
+    "DirCreatedEvent",
+    "DirDeletedEvent",
+    "DirMovedEvent",
+    "DirModifiedEvent",
+    "CustomHandler",
+    "start_watchdog",
+]
+
+
+# _SYNC_PRINT_TEMPLATE = r"""
+# Syncing:
+# D+ {dirs_created:5} / D- {dirs_deleted:5} / Dmod {dirs_modified:5} / Dmov {dirs_moved:5}
+# F+ {files_created:5} / F- {files_deleted:5} / Fmod {files_modified:5} / Fmov {files_moved:5}
+# """.strip()
+
+
+# --- Logics ---
 
 
 class CustomHandler(FileSystemEventHandler):
@@ -97,7 +123,7 @@ class CustomHandler(FileSystemEventHandler):
 
         for cb in self._table.get(
             event.__class__.__name__,
-            self._global_cb if self._global_cb else [self._default_event_cb]
+            self._global_cb if self._global_cb else [self._default_event_cb],
         ):
             cb(event)
 
@@ -109,7 +135,9 @@ class CustomHandler(FileSystemEventHandler):
         """Syntax sugar for register"""
         self.register(FileDeletedEvent, callback)
 
-    def register_on_file_modification(self, callback: Callable[[FileModifiedEvent], None]):
+    def register_on_file_modification(
+        self, callback: Callable[[FileModifiedEvent], None]
+    ):
         """Syntax sugar for register"""
         self.register(FileModifiedEvent, callback)
 
@@ -117,15 +145,21 @@ class CustomHandler(FileSystemEventHandler):
         """Syntax sugar for register"""
         self.register(FileMovedEvent, callback)
 
-    def register_on_directory_creation(self, callback: Callable[[DirCreatedEvent], None]):
+    def register_on_directory_creation(
+        self, callback: Callable[[DirCreatedEvent], None]
+    ):
         """Syntax sugar for register"""
         self.register(DirCreatedEvent, callback)
 
-    def register_on_directory_deletion(self, callback: Callable[[DirDeletedEvent], None]):
+    def register_on_directory_deletion(
+        self, callback: Callable[[DirDeletedEvent], None]
+    ):
         """Syntax sugar for register"""
         self.register(DirDeletedEvent, callback)
 
-    def register_on_directory_modification(self, callback: Callable[[DirModifiedEvent], None]):
+    def register_on_directory_modification(
+        self, callback: Callable[[DirModifiedEvent], None]
+    ):
         """Syntax sugar for register"""
         self.register(DirModifiedEvent, callback)
 
@@ -134,20 +168,59 @@ class CustomHandler(FileSystemEventHandler):
         self.register(DirMovedEvent, callback)
 
 
+# def sync_directories(
+#     src: pathlib.Path, dest: pathlib.Path, exclude_exts: set[str] = None
+# ):
+#     """Syncs src directory to dst directory recursively and slightly intelligently.
+#
+#     Args:
+#         src: source path.
+#         dest: destination path.
+#         exclude_exts: excluded file extensions from source path. Dest's remains will always be cleaned
+#     """
+#
+#     src_snap = DirectorySnapshot(src.as_posix())
+#     dest_snap = DirectorySnapshot(dest.as_posix())
+#
+#     diff = DirectorySnapshotDiff(src_snap, dest_snap)
+#
+#     print(_SYNC_PRINT_TEMPLATE.format_map(diff.__dict__))
+#
+#     # del
+#     for deleted in diff.files_deleted:
+#
+#         pathlib.Path(deleted).unlink()
+#
+#     for deleted in diff.dirs_deleted:
+#         pathlib.Path(deleted).rmdir()
+#
+#     # create
+#     for created in diff.files_created:
+#         pathlib.Path(created)
+
+
+# --- Drivers ---
+
+
 @contextmanager
-def start_watchdog(path: str, recursive: bool) -> Iterator[CustomHandler]:
+def start_watchdog(
+    watch_paths: Iterable[str], recursive: bool
+) -> Iterator[CustomHandler]:
     """Wraps the start & stop of watchdog observer and yields the handler."""
 
     observer = Observer()
     handler = CustomHandler()
-    observer.schedule(handler, path, recursive=recursive)
+
+    for path in watch_paths:
+        observer.schedule(handler, path, recursive=recursive)
+
     observer.start()
 
-    print(f"Watchdog started for: {path}")
+    print(f"Watchdog started for: {watch_paths}")
 
     yield handler
 
-    print(f"Watchdog stopping for: {path}")
+    print(f"Watchdog stopping for: {watch_paths}")
 
     observer.stop()
     observer.join()
